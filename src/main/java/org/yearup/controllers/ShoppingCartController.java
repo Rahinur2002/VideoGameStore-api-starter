d629cae9bd66b3ec73aff252349f323119d72fb9
Rahinur2002 <miahrahinur2002@gmail.com>
2025-12-18 08:42:35 -0500

    Implemented deleteProduct in MySqlShoppingDao to allow the user to delete products in their cart.

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -15,14 +15,13 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
-// only logged in users should have access to these actions
+
+// only logged in users have access to these actions
 @RestController
 @RequestMapping("cart")
 @CrossOrigin
 public class ShoppingCartController
 {
-    // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
@@ -34,16 +33,14 @@ public class ShoppingCartController
         this.productDao = productDao;
     }
 
-    // each method in this controller requires a Principal object as a parameter
     @GetMapping("")
     @PreAuthorize("hasRole('ROLE_USER')")
     public ShoppingCart getCart(Principal principal)
     {
         try
         {
-            // get the currently logged in username
+
             String userName = principal.getName();
-            // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
@@ -59,8 +56,8 @@ public class ShoppingCartController
         }
     }
 
-    // add a POST method to add a product to the cart - the url should be
-    // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    //add a product to the cart
+    //https://localhost:8080/cart/products/15
     @PostMapping("/products/{productId}")
     @PreAuthorize("hasRole('ROLE_USER')")
     @ResponseStatus(HttpStatus.CREATED)
@@ -77,9 +74,8 @@ public class ShoppingCartController
         return shoppingCartDao.addToCart(user.getId(), productId);
     }
 
-    // add a PUT method to update an existing product in the cart - the url should be
-    // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
-    // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    //update an existing product in the cart
+    // https://localhost:8080/cart/products/15
     @PutMapping("/products/{productId}")
     @PreAuthorize("hasRole('ROLE_USER')")
     public ShoppingCart updateCart (@PathVariable int productId,@RequestBody ShoppingCartItem item, Principal principal) {
@@ -97,7 +93,7 @@ public class ShoppingCartController
     }
 
 
-    // add a DELETE method to clear all products from the current users cart
+    //clear all products from the current users cart
     // https://localhost:8080/cart
     @DeleteMapping()
     @PreAuthorize("hasRole('ROLE_USER')")
