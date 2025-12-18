dc609bff586f7f9430162a72eb4d2ed466cca3ac
Rahinur2002 <miahrahinur2002@gmail.com>
2025-12-18 09:17:28 -0500

    Created MySqlProfileDao java class and implented getByUserId method to get the id of the user thats currently logged in

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.models.Profile;
 import org.yearup.data.ProfileDao;
-import org.yearup.models.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
